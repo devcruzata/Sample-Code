3f5c0f47032eb9b60aec1b22d394e455071a16cb
dev@cruzata <dev.cruzata@gmail.com>
2017-06-24 14:32:55 +0530

    DatabaseRepo

---
+++ DB_CONSTANTS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace DAL
+{
+    public class DB_CONSTANTS
+    {
+        private static string connString_Easy_Save = ConfigurationManager.AppSettings["connection_Easy_Save"].ToString();       ;
+
+
+        public static string ConnectionString_Easy_Save
+        {
+            get { return connString_Easy_Save; }
+        }       
+    }
+}
